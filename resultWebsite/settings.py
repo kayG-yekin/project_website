893783be976274679b8fbc417071b96c87689f70
Yeshidema <12190106.gcit@rub.edu.bt>
2022-05-25 00:10:52 +0600

    second commit

--- resultWebsite/settings.py
+++ resultWebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nr3-zhtk4v3tmo+f#n7mjw1lc@^6so&hf9hl_j+&6ioi^0^5p+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','resultwebsite.herokuapp.com/']
 
 
 # Application definition
